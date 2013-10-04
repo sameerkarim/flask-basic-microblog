dded9925ac84e23c44e62bedd4b6aac7f91fa102
Sameer Karim <sameerkarim1@gmail.com>
2013-10-04 21:22:26 +0800

    Fixed errant character in Post class

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post %r>' % (self.body) 
